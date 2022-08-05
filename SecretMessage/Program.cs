41c487ee2d85b825a4fbcde1c35e56b2dc8df9d3
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-05 20:08:25 +0300

    completed the first additional task

---
+++ SecretMessage/Program.cs
@@ -0,0 +1,29 @@
+﻿string message = "Какой чудесный день ";
+string password = "123qwe";
+int inputAttempt = 1;
+
+
+
+    
+while (inputAttempt <= 3)
+{
+    inputAttempt++;
+    Console.Write("Введите пароль ");
+    string inputPassword = Console.ReadLine();
+    if (inputPassword == password)
+{
+    Console.WriteLine(message);
+    break ;
+}
+else if (inputPassword != password)
+{
+    Console.WriteLine("Вы ввели неверный пароль. Попробуйте еще раз: ");
+}
+if (inputAttempt > 3)
+{
+    Console.WriteLine("Превышенно колличество попыток ");
+}
+}
+    
+
+    
