5e365437083dd05d385e922f64bdf54f1d3b9842
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-05 19:30:04 +0300

    Completed the fourth task

---
+++ find all even numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int even = 0;
+
+ while (number > even)
+ {
+    even++;
+    if (even % 2 == 0)
+    {
+        
+        Console.Write(" " + even);
+    }
+    
+ }
