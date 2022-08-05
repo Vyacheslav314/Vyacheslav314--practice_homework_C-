00106775358a9a2f07cd7e18d7f2d8bcf18d4091
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-05 18:40:30 +0300

    Completed the second task

---
+++ search for a larger number/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое числ: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber && firstNumber > thirdNumber)
+{
+    Console.Write("Число " + firstNumber + " Самое большое");
+}
+else if (firstNumber < secondNumber && secondNumber > thirdNumber)
+{
+    Console.Write("Число " + secondNumber + " Самое большое");
+}
+else if (thirdNumber > secondNumber && firstNumber < thirdNumber)
+{
+    Console.Write("Число " + thirdNumber + " Самое большое");
+}
+else
+{
+    Console.Write("Все числа равны");
+}
