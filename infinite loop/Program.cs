13923d0576032ca0a708f93627313961a3650aba
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-05 20:38:28 +0300

    completed the second additional task

---
+++ infinite loop/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Что бы завершить цикл введите exit ");
+string endCycle = Console.ReadLine();
+
+while (endCycle != "exit")
+{
+    endCycle = Console.ReadLine();
+    if (endCycle == "exit")
+    {
+        Console.WriteLine("УСПЕХ!!!!! ");
+    }
+    
+}
