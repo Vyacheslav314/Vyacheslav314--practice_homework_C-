e621e8c6b0bf45c8e7eaf2b5c6aa61fac8e16b40
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-05 18:36:24 +0300

    Completed the first task

---
+++ find_the_greater_of_2_numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое числ: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.Write("Первое число " + firstNumber + " Больше чем " + "Второе число " + secondNumber);
+}
+else if(firstNumber < secondNumber)
+{
+     Console.Write("Второе  число " + secondNumber + " Больше чем " + "Первое число " + firstNumber);
+}
+else
+{
+     Console.Write("Второе число " + secondNumber + " Равно " + "Первому числу " + firstNumber);
+}
