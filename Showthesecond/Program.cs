1c95612a4d6dcb07afba31992041a95d34d4f402
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-08 22:23:08 +0300

    complited HM day 2

---
+++ Showthesecond/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = ReadInt("Введите число от 100 до 999: ");
+
+Console.Write(ShowToSecond(number));
+
+int ShowToSecond(int num)
+{
+    int div = num / 10;
+    int remainderDiv = div % 10;
+    return remainderDiv;
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);   
+    return Convert.ToInt32(Console.ReadLine());
+}
