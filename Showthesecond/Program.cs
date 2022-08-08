95d2dbc2d2f66d1af2842b32853172d9b297bde4
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-08 22:33:01 +0300

    add corections

--- Showthesecond/Program.cs
+++ Showthesecond/Program.cs
@@ -1,6 +1,14 @@
 ﻿int number = ReadInt("Введите число от 100 до 999: ");
+int result = ShowToSecond(number);
 
-Console.Write(ShowToSecond(number));
+if (number > 100 && number < 1000)
+{
+    Console.Write(result);
+}
+else
+{
+    Console.Write("Введено некорректное число: ");
+}
 
 int ShowToSecond(int num)
 {
@@ -12,6 +20,6 @@ int ShowToSecond(int num)
 
 int ReadInt(string message)
 {
-    Console.Write(message);   
+    Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
