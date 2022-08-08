6720bec7c7d9d9a9702febc8d6f4600afec08aca
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-08 16:49:34 +0300

    add Game

---
+++ guessing game/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите число от 0 до 10 ");
+int number = Convert.ToInt32(Console.ReadLine());
+Random rnd = new Random();
+int value = rnd.Next(0, 10);
+int numberAttempts = 3;
+int index = 1;
+
+while (index != numberAttempts)
+{
+    if (number > value)
+    {
+        Console.WriteLine(number + " Больше загаданного числа");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    else if (number < value)
+    {
+        Console.WriteLine(number + " меньше загаданного числа");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    else
+    {
+        Console.WriteLine("Поздравляю это правельный ответ ");
+        break;
+    }     
+    index++;
+    if (index == numberAttempts)
+    {
+        Console.WriteLine("Превышенно количество попыток ");
+    }   
+}
+
+
