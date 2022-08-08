3ea1754aa5b0628eb6caaaa92c1a943f6073ac78
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-09 00:29:09 +0300

    modern

--- guessing game/Program.cs
+++ guessing game/Program.cs
@@ -1,21 +1,19 @@
-﻿Console.WriteLine("Введите число от 0 до 10 ");
-int number = Convert.ToInt32(Console.ReadLine());
-Random rnd = new Random();
-int value = rnd.Next(0, 10);
+﻿int number = ReadInt("Введите число от 0 до 10 ");
+int value = new Random().Next(0, 10);
 int numberAttempts = 3;
 int index = 1;
-Console.WriteLine(value);
+
 while (index <= numberAttempts)
 {
     index++;
     if (number > value)
     {
-        Console.WriteLine(number + " Больше загаданного числа");
+        Console.WriteLine(number + " Больше загаданного числа." + " Поробуйте еще раз: ");
         number = Convert.ToInt32(Console.ReadLine());
     }
-    else 
+    if (number < value)
     {
-        Console.WriteLine(number + " меньше загаданного числа");
+        Console.WriteLine(number + " меньше загаданного числа." + " Поробуйте еще раз: ");
         number = Convert.ToInt32(Console.ReadLine());
     }
     if (number == value) 
@@ -27,6 +25,11 @@ while (index <= numberAttempts)
     {
         Console.WriteLine("Превышенно количество попыток ");
         break;
-    }
-    
+    }  
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);   
+    return Convert.ToInt32(Console.ReadLine());
 }
