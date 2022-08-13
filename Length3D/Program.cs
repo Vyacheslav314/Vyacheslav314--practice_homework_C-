0e65bc018cc2fe4ac2f65517a8ffc6955e496b3b
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-13 16:37:41 +0300

    add hw task 9

---
+++ Length3D/Program.cs
@@ -0,0 +1,18 @@
+﻿int aX = ReadInt("Введите координаты первой точки по оси х: "); 
+int aY = ReadInt("Введите координаты первой точки по оси у: "); 
+int aZ = ReadInt("Введите координаты первой точки по оси z: ");  
+int bX = ReadInt("Введите координаты второй точки по оси х: "); 
+int bY = ReadInt("Введите координаты второй точки по оси у: "); 
+int bZ = ReadInt("Введите координаты второй точки по оси z: ");
+
+int lengthX = aX - bX;
+int lengthY = aY - bY;
+int lengthZ = aZ - bZ;
+double distance = Math.Sqrt(lengthX * lengthX + lengthY * lengthY + lengthZ * lengthZ);
+Console.WriteLine(distance);
+
+int ReadInt(string message)
+{
+    Console.Write(message);   
+    return Convert.ToInt32(Console.ReadLine());
+}
