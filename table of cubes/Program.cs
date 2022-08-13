c813ee3aca831046e3e21d601f1fb0fbad5cff5f
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-13 16:42:41 +0300

    add hw task 10

---
+++ table of cubes/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = ReadInt("Введите число n: ");
+
+for(int i = 1; i <= number; i++)
+{
+int resultCubes = i * i * i;
+Console.Write(resultCubes + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);   
+    return Convert.ToInt32(Console.ReadLine());
+}
