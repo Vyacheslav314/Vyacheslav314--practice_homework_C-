26b9d33b82dda67181af18769e8f280dc7a84e4c
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-13 16:18:03 +0300

    fix

--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -1,17 +1,22 @@
-﻿int number = ReadInt("введите число для проверки его на палиндром: ");
+﻿int number = ReadInt("введите пятизначное число: ");
 int temp = number;
 int number1 = 0;
 int sum = 0;
+int index = 0;
 
 while(number != 0)
 {
     number1 = number % 10;
     number = number / 10;
     sum = sum * 10 + number1;
+    index++;
     
 }
-
-if(sum == temp)
+if(index > 5 || index < 5)
+{
+    Console.Write("Число не пятизначное ");
+}
+else if(sum == temp)
 {
     Console.Write("палиндром");
 }
