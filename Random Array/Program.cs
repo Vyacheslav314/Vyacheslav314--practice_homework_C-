897c1b4e7626cb00d54c2270093ec126312a2e88
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-16 21:26:12 +0300

    add 13 hw

---
+++ Random Array/Program.cs
@@ -0,0 +1,27 @@
+﻿int size = ReadInt("Укажите длину массива ");
+int[] array = new int[size];
+
+FillingArray(array, size);
+PrintArray(array);
+
+void PrintArray(int[] arr1)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(arr1[i] + " ");
+    }
+}
+
+void FillingArray(int[] arr, int sizeArray)
+{
+    for (int i = 0; i < sizeArray; i++)
+    {
+        arr[i] = ReadInt("Введите слeдующий элемент массива ");
+    }
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
