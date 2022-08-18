63295516b8fc514719ba01f9a3b1862e24113446
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-18 22:59:41 +0300

    hw 15

---
+++ SumNotEvenElem/Program.cs
@@ -0,0 +1,45 @@
+﻿int size = ReadCommand("Укажите длину массива ");
+int[] array = new int[size];
+
+FillRandomArray(array);
+CounterNotEvenElemArray(array);
+PrintArray(array);
+
+
+int CounterNotEvenElemArray(int[] arr)
+{
+    int sumElem = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sumElem += arr[i];
+        }
+    }
+    Console.WriteLine(sumElem);
+    return sumElem;
+}
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+void FillRandomArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-10, 100);
+    }
+}
+
+
+int ReadCommand(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
