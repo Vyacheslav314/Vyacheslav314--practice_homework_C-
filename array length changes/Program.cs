58b7fd1142675f23d8cb30f265e7978ab9f850d4
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-21 18:48:08 +0300

    Convert v1.01

---
+++ array length changes/Program.cs
@@ -0,0 +1,54 @@
+﻿int[] array = { 1, 5, 7, 8 };
+
+PrintArray(array);
+Console.WriteLine();
+PrintArray(AddToArray(array));
+Console.WriteLine();
+PrintArray(RemoveFromArray(array));
+
+int[] RemoveFromArray(int[] arr)
+{
+    int[] newArray = new int[arr.Length - 1];
+    int number = ReadCommand("Укажите индекс который нужно удалить ");
+    for (int i = 0; i < number; i++)
+    {
+        newArray[i] = arr[i];
+    }
+    for (int j = number + 1; j < newArray.Length + 1; j++)
+    {
+        newArray[j - 1] = arr[j];
+    }
+    return newArray;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int[] AddToArray(int[] arr)
+{
+    int[] newArray = new int[arr.Length + 1];
+    int number = ReadCommand("Введите число которое хотите добавить в массив ");
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        for (int j = 0; j < arr.Length; j++)
+        {
+            if (i >= j)
+            {
+                newArray[i] = arr[j];
+            }
+        }
+        newArray[arr.Length] = number;
+    }
+    return newArray;
+}
+
+int ReadCommand(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ tetris/Program.cs
@@ -0,0 +1,68 @@
+﻿int[,] tetris = new int[,]
+{
+    { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+};
+
+PrintArray(tetris);
+MovementFigures(1, 7);
+
+void MovementFigures(int row, int col)
+{
+    if (tetris[row, col] == 0)
+    {
+        tetris[row, col] = 1;
+        int comm = Convert.ToInt32(Console.ReadLine());
+        PrintArray(tetris);
+        bool isValid = true;
+        while(isValid)
+        {
+        switch (comm)
+        {
+            case 1:
+                MovementFigures(row++, col++);
+                PrintArray(tetris);
+                break;
+            case 2:
+                MovementFigures(row++, col--);
+                
+                PrintArray(tetris);
+                break;
+                case 3:
+                isValid = false;
+                break;
+        }
+        }
+    }
+}
+
+void PrintArray(int[,] tet)
+{
+    for (int i = 0; i < tet.GetLength(0); i++)
+    {
+        for (int j = 0; j < tet.GetLength(1); j++)
+        {
+            if (tet[i, j] == 0)
+            {
+                Console.Write($" ");
+            }
+            else
+            {
+                Console.Write($"+");
+            }
+        }
+        Console.WriteLine();
+    }
+}
