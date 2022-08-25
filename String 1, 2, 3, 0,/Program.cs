1730b3a7471e8c294cc67a36300aecd2f3a804af
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-25 18:07:14 +0300

    hw 41fix

--- String 1, 2, 3, 0,/Program.cs
+++ String 1, 2, 3, 0,/Program.cs
@@ -1,49 +1,33 @@
 ﻿string separatedNumbers = ReadCommand("Введите числа через запятую: ");
 string[] substring = new string[separatedNumbers.Length];
-int[] numbers = new int[substring.Length];
 int startIndex = 0;
-int numberlength = 0;
 int count = 0;
 
-for (int i = 0; i < separatedNumbers.Length; i++)
+WritingStringArray(substring, separatedNumbers);
+
+for (int i = 0; i < substring.Length; i++)
 {
-    numberlength++;
-    if (separatedNumbers[i] != ',')
+    if(Convert.ToInt32(substring[i]) > 0)
     {
-        for (int j = startIndex; j < numberlength; j++)
+        count++;
+    }
+}
+Console.WriteLine(count);
+
+void WritingStringArray(string[] Array, string number)
+{
+    for (int i = 0; i < separatedNumbers.Length; i++)
+    {
+        if (separatedNumbers[i] != ',')
         {
-            
-            substring[startIndex] += Convert.ToString(separatedNumbers[i]);
-            j++;
+            substring[startIndex] += separatedNumbers[i];
+        }
+        else
+        {
+            startIndex++;
         }
     }
-    if(separatedNumbers[i] == ',')
-    {
-    startIndex = i;
-    }
 }
-
-for(int i = 0; i < numbers.Length; i++)
-{
-    numbers[i] = Convert.ToInt32(substring[i]);
-
-}
-
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] > 0) count++;
-}
-
-PrintArray(substring);
-
-void PrintArray(string[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.Write(arr[i] + ".");
-    }
-}
-
 
 string ReadCommand(string message)
 {
