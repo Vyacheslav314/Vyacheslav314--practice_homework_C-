c3c218f4a0c6083e60cafb9808e95d2530a43d3e
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-25 23:15:24 +0300

    hw 41fixv2

--- String 1, 2, 3, 0,/Program.cs
+++ String 1, 2, 3, 0,/Program.cs
@@ -1,21 +1,45 @@
 ﻿string separatedNumbers = ReadCommand("Введите числа через запятую: ");
-string[] substring = new string[separatedNumbers.Length];
-int startIndex = 0;
+int countComma = CommaCounter(separatedNumbers);
+string[] substring = new string[countComma];
 int count = 0;
 
 WritingStringArray(substring, separatedNumbers);
+PrintArray(substring);
+
+int CommaCounter(string number)
+{
+    int count = 1;
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (number[i] == ',')
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 for (int i = 0; i < substring.Length; i++)
 {
-    if(Convert.ToInt32(substring[i]) > 0)
+    if (Convert.ToInt32(substring[i]) > 0)
     {
         count++;
     }
 }
-Console.WriteLine(count);
 
-void WritingStringArray(string[] Array, string number)
+Console.WriteLine($"Елементов в троке больше 0 ==> {count} ");
+
+void PrintArray(string[] arr)
 {
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+void WritingStringArray(string[] array, string number)
+{
+    int startIndex = 0;
     for (int i = 0; i < separatedNumbers.Length; i++)
     {
         if (separatedNumbers[i] != ',')
