a245079b0f69caad73e93c86e04e592377f9fae6
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-25 23:18:35 +0300

    hw lines

---
+++ intersection of straight lines/Program.cs
@@ -0,0 +1,24 @@
+﻿double b1 = ReadCommand("Введите точку b1: ");
+double b2 = ReadCommand("Введите точку b2: ");
+double k1 = ReadCommand("Введите точку k1: ");
+double k2 = ReadCommand("Введите точку k2: ");
+double x = (b2 - b1) / (k1 - k2);
+if(k1 != k2)
+{
+double y = k1 * x + b1;
+double y1 = k2 * x + b2;
+
+Console.WriteLine(y1);
+Console.WriteLine(y);
+}
+else
+{
+    Console.Write("Прямыe не пересекаются!!!");
+}
+
+
+double ReadCommand(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
